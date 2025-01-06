593114cb2651edc490d2e2321b96032e71194fe0
gustberchmans <gustberchmans@gmail.com>
2025-01-06 12:39:21 +0100

    Revert "admin kan user admin geven en ontnemen"
    This reverts commit 938b4edaff142ada1d28b4380c13ec6a60a82893.

--- app/Http/Kernel.php
+++
@@ -1,4 +0,0 @@
-protected $routeMiddleware = [
-    // andere middleware
-    'admin' => \App\Http\Middleware\AdminMiddleware::class, // Voeg deze regel toe
-];
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,12 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
-    Route::post('/admin/users/{user}/promote', [UserController::class, 'promote'])->name('admin.promote');
-    Route::post('/admin/users/{user}/demote', [UserController::class, 'demote'])->name('admin.demote');
 });
 
 require __DIR__.'/auth.php';
